9fb5bb598781f2b4e06c88ea3dbab7655faf2aa8
LiuYang <fyeeme@gmail.com>
2022-02-03 20:53:33 +0800

    feat: add spring security demo

---
+++ spring-securty/src/main/java/com/lorraine/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.lorraine.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig {
+
+    @Bean
+    WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/webjars/**");
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.cors().disable().build();
+    }
+}
---
+++ spring-securty/src/main/java/com/lorraine/security/user/UserController.java
@@ -0,0 +1,25 @@
+package com.lorraine.security.user;
+
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    @PermitAll
+    public String search() {
+        return "hello, world!";
+    }
+
+    @GetMapping("/{id}")
+    @RolesAllowed("USER")
+    public Long get(@PathVariable Long id) {
+        return id;
+    }
+}
